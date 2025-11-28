b1003b0df4b5109c7cacd415826958dacc1fb37f
sree <sree@buckyball>
2025-11-28 00:26:33 -0500

    Get sha256_init working (from edit-distance)

--- gpucodeanalyzer/isa/sass/sass2c.py
+++ gpucodeanalyzer/isa/sass/sass2c.py
@@ -8,27 +8,28 @@ class SASS2C:
 
     def declare_registers(self):
         self.output.write("    const sass_reg RZ = 0;\n")
+        self.output.write("    const sass_reg URZ = 0;\n")
         self.output.write("    const sass_predicate_reg PT = 1;\n")
 
-        declared = set(['RZ', 'PT'])
+        declared = set(['RZ', 'PT', 'URZ'])
         for i in self.cfg.all_instructions():
             for x in i.args:
                 if isinstance(x, SASSRegister):
                     n = x.n
                     if n not in declared:
-                        if n.startswith("R"):
+                        if n.startswith("R") or n.startswith("UR"):
                             self.output.write(f"    sass_reg {n};\n")
                             declared.add(n)
                         elif n.startswith('P'):
                             self.output.write(f"    sass_predicate_reg {n};\n")
                             declared.add(n)
                         elif n.startswith('SR_CTAID'):
-                            self.output.write(f"    sass_vec3 SR_CTAID;\n")
+                            #self.output.write(f"    sass_vec3 SR_CTAID;\n")
                             declared.add('SR_CTAID.X')
                             declared.add('SR_CTAID.Y')
                             declared.add('SR_CTAID.Z')
                         elif n.startswith('SR_TID'):
-                            self.output.write(f"    sass_vec3 SR_TID;\n")
+                            #self.output.write(f"    sass_vec3 SR_TID;\n")
                             declared.add('SR_TID.X')
                             declared.add('SR_TID.Y')
                             declared.add('SR_TID.Z')
@@ -51,17 +52,30 @@ class SASS2C:
         self.output.write("#include <stdbool.h>\n")
         self.output.write("#include <stdio.h>\n")
         self.output.write('#include "sass_insns.h"\n\n')
+        self.output.write('#include "lop3_lut.h"\n\n')
 
         self.output.write("typedef uint32_t sass_reg;\n")
         self.output.write("typedef bool sass_predicate_reg;\n")
         self.output.write("typedef struct { sass_reg X; sass_reg Y; sass_reg Z; } sass_vec3;\n\n")
         self.output.write("// cfg\n")
         func_name = "default_sass_name" # until we get support for functions
-        args = [] # need to work this out
+        args = ['sass_vec3 GRID_DIM', 'sass_vec3 CTA_DIM'] # need to work this out
         self.output.write(f"void {func_name}({','.join(args)}) {{\n")
 
-        self.declare_registers()
         self.declare_counts()
+        self.output.write(f"    sass_vec3 SR_CTAID;\n")
+        self.output.write(f"    sass_vec3 SR_TID;\n")
+        
+        self.output.write("for(SR_CTAID.Z=0; SR_CTAID.Z<GRID_DIM.Z; SR_CTAID.Z++) {\n")
+        self.output.write("for(SR_CTAID.Y=0; SR_CTAID.Y<GRID_DIM.Y; SR_CTAID.Y++) {\n")
+        self.output.write("for(SR_CTAID.X=0; SR_CTAID.X<GRID_DIM.X; SR_CTAID.X++) {\n")
+
+        self.output.write("  for(SR_TID.Z=0; SR_TID.Z<CTA_DIM.Z; SR_TID.Z++) {\n")
+        self.output.write("  for(SR_TID.Y=0; SR_TID.Y<CTA_DIM.Y; SR_TID.Y++) {\n")
+        self.output.write("  for(SR_TID.X=0; SR_TID.X<CTA_DIM.X; SR_TID.X++) {\n")
+
+        self.declare_registers()
+
 
     def output_block_order(self):
         order = [('_start', -1)]
@@ -79,7 +93,10 @@ class SASS2C:
 
     def xlat_insn(self, i):
         def process_c_lookup(cl):
-            return cl
+            if cl == "c[0x0][0x0]":
+                return "GRID_DIM.X" # webgpu only?
+            else:
+                return None
 
         def process_cx_lookup(cx):
             return cx
@@ -93,13 +110,18 @@ class SASS2C:
                     if a.startswith('-') or a.startswith('0x'):
                         o.append(f"(sass_reg) {a}")
                     elif a.startswith('c['):
-                        self._xlat_failure('constant')
-                        return None
-                        o.append(process_c_lookup(a))
+                        c = process_c_lookup(a)
+                        if c:
+                            o.append(c)
+                        else:
+                            self._xlat_failure('constant')
+                            return None
                     elif a.startswith('cx['):
                         self._xlat_failure('cx')
                         return None
                         o.append(process_cx_lookup(a))
+                    else:
+                        raise NotImplementedError(a)
 
             return ', '.join(o)
 
@@ -115,6 +137,14 @@ class SASS2C:
             opcode = "IADD3"
         elif i.opcode == "EXIT":
             opcode = "EXIT"
+        elif i.opcode == "UMOV":
+            opcode = "UMOV"
+        elif i.opcode == "UIADD3":
+            opcode = "UIADD3"
+        elif i.opcode == "LOP3.LUT": # other variants not yet supported, see ptx
+            opcode = "LOP3_LUT"
+            assert i.args[-1] == "!PT", i.args[-1]
+            args = process_args(i.args[:-1])
         elif i.opcode.startswith("ISETP."):
             cvtop = i.opcode.replace('.', '_')
             opcode = [cvtop + "_D0"]
@@ -168,6 +198,7 @@ class SASS2C:
         self.output.write("\n")
 
     def finish_cfg(self):
+        self.output.write("}}}}}}\n")
         self.output.write("label_exit:\n")
         for c in self.counters:
             self.output.write(f'    printf("{c} = %lu\\n", {c});\n')
--- gpucodeanalyzer/isa/sass/sass_insns.h
+++ gpucodeanalyzer/isa/sass/sass_insns.h
@@ -9,10 +9,11 @@
 #define IMAD_MOV_U32(dst, src1, src2, src3) IMAD(dst, src1, src2, src3)
 #define IMAD_IADD_U32(dst, src1, src2, src3) IMAD(dst, src1, src2, src3)
 #define IMAD_SHL_U32(dst, src1, src2, src3) IMAD(dst, src1, src2, src3)
+#define IMAD_MOV(dst, src1, src2, src3) IMAD(dst, src1, src2, src3)
 
 #define IADD3(dst, src1, src2, src3) dst = src1 + src2 + src3
 
-#define EXIT() goto label_exit
+#define EXIT() continue
 #define BRA(label) goto label
 
 #define ISETP_GT_AND_D0(dst0, dst1, src1, src2, src3) dst0 = (src1 > src2) && src3
@@ -20,3 +21,13 @@
 
 #define ISETP_GE_AND_D0(dst0, dst1, src1, src2, src3) dst0 = (src1 >= src2) && src3
 #define ISETP_GE_AND_D1(dst0, dst1, src1, src2, src3) dst1 = !dst0
+
+#define ISETP_GE_U32_AND_D0(dst0, dst1, src1, src2, src3) dst0 = (src1 >= src2) && src3
+#define ISETP_GE_U32_AND_D1(dst0, dst1, src1, src2, src3) dst1 = !dst0
+
+#define ISETP_NE_U32_AND_D0(dst0, dst1, src1, src2, src3) dst0 = (src1 != src2) && src3
+#define ISETP_NE_U32_AND_D1(dst0, dst1, src1, src2, src3) dst1 = !dst0
+
+#define UMOV(dst, src) dst = src
+#define UIADD3(dst, src1, src2, src3) dst = src1 + src2 + src3
+#define LOP3_LUT(dst, src1, src2, src3, immLut) dst = logical_op3(src1, src2, src3, immLut)
