0b93afcd3a9dd874929a1165d42c09822432c081
sree <sree@buckyball>
2025-11-29 01:00:50 -0500

    Enhance read/write sets

--- gpucodeanalyzer/isa/sass/sass.py
+++ gpucodeanalyzer/isa/sass/sass.py
@@ -2,22 +2,46 @@ import re
 from ...generic_cfg import Instruction, ControlInsn, Register, Memory
 
 SASS_INSN_RE = re.compile(r"^\s*/\*([0-9a-f]+)\*/\s+(.+) ;$")
-SASS_REG_RE = re.compile(r"(((UR|R|!?P|B|UP)\d+)(.reuse)?)|(UPT|PT|RZ|URZ|SRZ|SR_CTAID\.?|SR_TID\.?)")
+SASS_REG_RE = re.compile(r"-?(((UR|R|!?P|B|UP)\d+)(.reuse)?)|(UPT|PT|RZ|URZ|SRZ|SR_CTAID\.?|SR_TID\.?)")
 
-CX_RE = re.compile(r"cx\[(?P<regbase>.+)\]\[(?P<offset>.+)\]")
+CX_RE = re.compile(r"-?cx\[(?P<regbase>.+)\]\[(?P<offset>.+)\]")
+
+CONSTANT_REGS = set(['RZ', 'SRZ', 'URZ', 'PT', 'UPT', 'SR_TID.X', 'SR_CTAID.X',
+                     'SR_TID.Y', 'SR_TID.Z', 'SR_CTAID.Y', 'SR_CTAID.Z'])
+
 
 class SASSRegister(Register):
-    def __init__(self, n, is_inverted = False):
+    def __init__(self, n, is_inverted = False, is_negated = False, is_reuse = False):
         super().__init__(n)
 
         # todo: reuse, !, .cc
         self.is_inverted = is_inverted
+        self.is_negated = is_negated
+        self.is_reuse = is_reuse
 
     def is_constant(self):
-        return self.n == "RZ" or self.n == "PT"
+        return self.n in CONSTANT_REGS
+
+    def operand(self):
+        n = self.n
+
+        if self.is_reuse:
+            n = n + ".reuse"
+
+        if self.is_inverted:
+            return "!" + n
+
+        if self.is_negated:
+            return "-" + n
+
+        return n
 
 class SASSInstruction(Instruction):
-    WRITE_COUNT = {'BSYNC': 0}
+    WRITE_COUNT = {'BSYNC': 0,
+                   ('IADD3', 5): 2}
+
+    MULTI_WRITER = {'LDG.E.128.STRONG.GPU': {0: 4}}
+
     def __init__(self, pc, pred, opcode, args, insn):
         self.label = pc
         self.predicate = pred
@@ -30,14 +54,20 @@ class SASSInstruction(Instruction):
             m = SASS_REG_RE.match(a)
             if m is not None:
                 r = None
+
+                # TODO: properly record is_inverted, is_negated, is_reuse, etc.
                 if a[0] == "!":
                     a = a[1:]
                     r = SASSRegister(a, is_inverted = True)
 
+                if a[0] == "-":
+                    a = a[1:]
+                    r = SASSRegister(a, is_negated = True)
+
                 if a.endswith(".reuse"):
                     assert r is None
                     a = a[:-len(".reuse")]
-                    r = SASSRegister(a) #TODO: is_reuse
+                    r = SASSRegister(a, is_reuse = True)
                 else:
                     r = SASSRegister(a)
 
@@ -52,14 +82,27 @@ class SASSInstruction(Instruction):
 
     __repr__ = __str__
 
+    def write_count(self):
+        if self.opcode in SASSInstruction.WRITE_COUNT:
+            write_args = SASSInstruction.WRITE_COUNT[self.opcode]
+        elif (self.opcode, len(self.args)) in SASSInstruction.WRITE_COUNT:
+            write_args = SASSInstruction.WRITE_COUNT[(self.opcode, len(self.args))]
+        else:
+            write_args = 1
+
+        return write_args
+
     def reads(self):
-        write_args = SASSInstruction.WRITE_COUNT.get(self.opcode, 1)
+        write_args = self.write_count()
         rds = list(x for x in self.args[write_args:] if isinstance(x, Register))
         if self.predicate:
             n = self.predicate
             if n[0] == "!": n = n[1:]
+
             rds.append(SASSRegister(n,
                                     is_inverted = self.predicate[0] == "!"))
+
+        # TODO: multi-readers
 
         # hack, need to fix this.
         for x in self.args[write_args:]:
@@ -71,8 +114,28 @@ class SASSInstruction(Instruction):
         return rds
 
     def writes(self):
-        write_args = SASSInstruction.WRITE_COUNT.get(self.opcode, 1)
-        return list(x for x in self.args[:write_args] if isinstance(x, Register) and not x.is_constant())
+        def extend(r, n):
+            assert r.n[0] == "R"
+            rno = int(r.n[1:])
+            return [SASSRegister(f"R{d}") for d in range(rno, rno+n)]
+
+        write_args = self.write_count()
+        writes = list(x for x in self.args[:write_args] if isinstance(x, Register) and not x.is_constant())
+
+        if self.opcode in SASSInstruction.MULTI_WRITER:
+            for arg, ext in SASSInstruction.MULTI_WRITER[self.opcode].items():
+                writes[arg] = extend(writes[arg], ext)
+
+            o = []
+            for x in writes:
+                if isinstance(x, list):
+                    o.extend(x)
+                else:
+                    o.add(x)
+
+            return o
+        else:
+            return writes
 
     @staticmethod
     def parse(insn):
