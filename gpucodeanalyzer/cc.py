f4a2ff1ffabf212496e148c2fef4ef4824d1c59d
Sreepathi Pai <sree@wapato>
2026-02-17 18:45:50 -0700

    First working version of gca-cc, needs some sort of config facility for paths

--- gpucodeanalyzer/cc.py
+++ gpucodeanalyzer/cc.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import shutil
+import subprocess
+import shlex
+import sys
+from pathlib import Path
+
+def get_compiler():
+    compiler = shutil.which(os.getenv('CC', 'cc'))
+    return compiler
+
+def get_flags():
+    return ["-Wmaybe-uninitialized"]
+
+def get_include_dirs():
+    return [Path(__file__).parent / '..' / 'src',
+            Path(__file__).parent / 'isa' / 'sass',
+            ]
+
+def get_lib_dirs():
+    return []
+
+def get_libs():
+    return []
+
+def get_objects():
+    return [Path(__file__).parent / '..' / 'src' / 'pathtrace.o']
 
 def main():
     p = argparse.ArgumentParser(description="Run compiler on gca-cfg2c code")
@@ -10,7 +37,22 @@ def main():
 
     args = p.parse_args()
 
+    cmd = [get_compiler()]
+
+    if not any([x.startswith("-O") for x in args.args]):
+        cmd.append("-O1") # needed for warnings for gcc
+
+    cmd.extend(get_flags())
+    if args.output:
+        cmd.extend(['-o', args.output])
+    cmd.extend([f"-I{dir}" for dir in get_include_dirs()])
+    cmd.extend([f"-L{dir}" for dir in get_lib_dirs()])
+    cmd.extend(args.args)
+    cmd.extend([f"-l{dir}" for dir in get_libs()])
+    cmd.extend([str(s) for s in get_objects()])
+
+    print(shlex.join(cmd), file=sys.stderr)
+    subprocess.run(cmd)
+
 if __name__ == "__main__":
     main()
-
-    
